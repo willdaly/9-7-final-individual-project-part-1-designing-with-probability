f623a9866ebcc752387a658f9c7b9d23a048fed3
Will <rwilliamdaly@gmail.com>
2026-03-19 20:45:07 -0400

    Add tests/test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""
+Tests for 9.7 Final Individual Project – Part 1: Designing With Probability
+"""
+import pytest
+from main import main
+
+
+def test_main():
+    """Test the main function."""
+    # TODO: Add your test cases here
+    pass
